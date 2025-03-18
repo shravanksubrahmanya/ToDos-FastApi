4c68331565251e72d3c761d024a026986ce33d0f
Shravan K Subrahmanya <shravanksubrahmanya@gmail.com>
2025-03-18 13:21:25 +0530

    add user creation endpoint with Pydantic validation and password hashing

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,33 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from passlib.context import CryptContext
+from sqlalchemy.util import deprecated
+
+from models import Users
 
 router = APIRouter()
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-@router.get("/auth/")
-async def get_user():
-    return {"message": "You are authenticated"}
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+@router.post("/auth/")
+async def create_user(create_user_request: CreateUserRequest):
+    # create_user_model = Users(**create_user_request.model_dump())
+    create_user_model = Users(
+        email = create_user_request.email,
+        username = create_user_request.username,
+        first_name = create_user_request.first_name,
+        last_name = create_user_request.last_name,
+        role = create_user_request.role,
+        hashed_password = bcrypt_context.hash(create_user_request.password),
+        is_active = True
+    )
+
+    return create_user_model
