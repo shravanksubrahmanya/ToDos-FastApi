80f73b97bff0e65f862e48c9daead75487f34bc7
Shravan K Subrahmanya <shravanksubrahmanya@gmail.com>
2025-03-22 16:32:33 +0530

    refactor database connection, improve import paths, and add health check test

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
-import models
+import sys
+import os
+
+# To ensure that your application uses absolute paths,
+# you can modify the sys.path in your main application file (main.py) to include the ToDos directory.
+# This way, all imports will be resolved correctly.
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+from models import base
 from database import engine
-from routers import auth
-from routers import todos
-from routers import admin
-from routers import users
+from routers import auth, todos, admin, users
+
 
 app = FastAPI()
 
-models.base.metadata.create_all(bind=engine)
+base.metadata.create_all(bind=engine)
 
 @app.get("/healthy")
 def health_check():
---
+++ test/test_main.py
@@ -0,0 +1,10 @@
+from starlette.testclient import TestClient
+from starlette import status
+from ToDos.main import app
+
+client = TestClient(app)
+
+def test_return_health_check():
+    response = client.get("/healthy")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"status": "Healthy"}
