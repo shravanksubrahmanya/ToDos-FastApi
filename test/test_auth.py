c1cffc78e7995b8f19467cb1d14a363568fe5806
Shravan K Subrahmanya <shravanksubrahmanya@gmail.com>
2025-03-23 12:22:14 +0530

    add tests for creating access tokens and clean up imports in auth module

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,7 +1,10 @@
 from jose import  jwt
 from datetime import timedelta
+import pytest
+from fastapi.exceptions import HTTPException
+
 from .utils import *
-from routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
+from routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM, get_current_user
 
 app.dependency_overrides[get_db()] = override_get_db()
 
@@ -30,4 +33,24 @@ def test_create_access_token():
     assert decoded_token['id'] == user_id
     assert decoded_token['role'] == role
 
+@pytest.mark.asyncio
+async def test_get_current_user_valid_token():
+    # pytest by default can not test asynchronous functionality, it can only test synchronous functionality.
+    encode = {'sub':'testuser', 'id':1, 'role':'admin'}
+
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    user = await get_current_user(token=token)
+    assert user == {'username':'testuser', 'user_id':1, 'user_role':'admin'}
+
+@pytest.mark.asyncio
+async def test_get_current_user_missing_payload():
+    encode = {'role':'user'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    with pytest.raises(HTTPException) as exinfo:
+        await get_current_user(token=token)
+
+    assert exinfo.value.status_code == 401
+    assert exinfo.value.detail == "Invalid Credentials"
 
